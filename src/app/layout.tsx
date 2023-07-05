0297e6d4e6be683d65aa1c71b06d2d3934ae6015
Umarkhon Akhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2023-07-05 11:24:52 +0500

    Creating Next Js app - initial steps

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+
+export const metadata = {
+  title: "Breadit",
+  description: "Social Media App",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialised">
+        <Navbar />
+        <div className="container max-w-7xl mx-auto pt-12">{children}</div>
+      </body>
+    </html>
+  );
+}
