21567212fd77fba40a91b3253cf3263891f5a103
Umarkhon Akhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2023-07-05 13:30:16 +0500

    Creating Auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className="min-h-screen pt-12 bg-slate-50 antialised">
         <Navbar />
         <div className="container max-w-7xl mx-auto pt-12">{children}</div>
+        <Toaster />
       </body>
     </html>
   );
